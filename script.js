c27ca5515769a72ba9adf3a2dc89e9371258a0c1
Bryan Cho <bryanc208@gmail.com>
2014-04-10 17:43:08 -0700

    everything is working so far hue

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
 var savedPictures = [];
 
-$(function(){
+var limit = 50;
 
+var key = "savedImageURL";
+
+
+$(function(){
+    //loading local storage-------------------------------------------------------------------
+    savedPictures = JSON.parse(localStorage[key]);
+    if (savedPictures === null) savedPictures = [];
+    console.log(JSON.parse(localStorage[key]));
+    $("#count").html(savedPictures.length);  
+    for(i in savedPictures){
+    $("#count").append($("<img>")
+                       .attr("src",savedPictures[i])
+                       .addClass("savedImage"));
+    } 
+    
+    //API call to imgur-----------------------------------------------------------------------
     $.ajax({
         url: 'https://api.imgur.com/3/gallery/hot/viral/0.json',
         type: 'GET',
         headers: { "Authorization": "Client-ID cb1bb6f9220bbe3" },
         success: function (data) {  
             for(i in data.data){
+                var src = data.data[i].link;
                 if(data.data[i].is_album === false){
                     var container =  $("<div>")
                         .html("<p>"+data.data[i].title+"</p>")
-                        .appendTo($("#imgcontainer"));
+                        .appendTo($("#imgurcontainer"));
                     $("<img>")
                         .addClass("waffleImage")
-                        .attr("src", data.data[i].link)
-                        .appendTo(container); 
+                        .attr("src", src)
+                        .appendTo(container);
+                    
+                }if($.inArray(src, savedPictures) !== (-1)){
+                    var pic = $('.row img[src="'+src+'"]');
+                    pic.parent().css("visibility", "hidden").css("position", "absolute");
                 }
             }
         },
@@ -24,29 +45,53 @@ $(function(){
         }
     });
     
-    $(document).on("dblclick", ".waffleImage", function(){
-        /*for(i in savedPictures){  
-        WHY DOES FUCKING "$(this).attr("src") in savedPictures" NOT WORK FUCK
-            if(savedPictures[i] === $(this).attr("src")){
-                savedPictures.splice(i,1);
-                $("#count").html(savedPictures.length);
-                $("img").filter(function(index){
-                    return $("src", this) === savedPictures[i];
-                }).remove()
-            }
-            savedPictures.push($(this).attr("src"));
-            $("#count").html(savedPictures.length);
-            $("#count").append($("<img>").attr("src",savedPictures[i]));
-            }
-        }*/
+    //API call to giphy------------------------------------------------------------------------------
+    var xhr = $.get("http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC&limit="+limit);
+    xhr.done(function(data){
+        for(i in data.data){
+            var container = $("<div>").appendTo("#giphycontainer");
+            var src = data.data[i].images.original.url;
+            $("<img>").addClass("waffleImage").attr("src", src).appendTo(container);
+            if($.inArray(src, savedPictures) !== (-1)){
+                var pic = $('.row img[src="'+src+'"]');
+                pic.parent().css("visibility", "hidden").css("position", "absolute");
+            }    
+        }
+    });
 
-            savedPictures.push($(this).attr("src"));
-            $("#count").html(savedPictures.length);
+    
+    //save picture----------------------------------------------
+    $(".row").on("dblclick", ".waffleImage", function(){
+        var src = $(this).attr('src');
+        if($('#count').find('[src="'+src+'"]').length === 0){
+            savedPictures.push(src);
+            $("#count").html(savedPictures.length);         
             for(i in savedPictures){
-                $("#count").append($("<img>").attr("src",savedPictures[i]));
-            }
-           
-        
+                $("#count").append($("<img>")
+                                   .attr("src",savedPictures[i])
+                                   .addClass("savedImage"));
+            }  
+        $(this).parent().css("visibility", "hidden").css("position", "absolute");
+        localStorage[key] = JSON.stringify(savedPictures);
+    } 
+    
+    
+    });
+    
+    //remove picture---------------------------------------------
+    $('#count').on("dblclick", ".savedImage", function(e){
+        $(this).remove();
+        var src = $(this).attr('src');
+        savedPictures.splice(savedPictures.indexOf(src,1));
+        $("#count").html(savedPictures.length);
+        for(i in savedPictures){
+                $("#count").append($("<img>")
+                                   .attr("src",savedPictures[i])
+                                   .addClass("savedImage"));
+        }
+        var pic = $('.row img[src="'+src+'"]');
+        pic.parent().css("visibility", "visible").css("position", "static");
+        localStorage[key] = JSON.stringify(savedPictures);
     });
 
     /*
